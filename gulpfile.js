d3259ae0aa73eaa3324279d1281e4d8af19ce599
Daniel Alves <daniel199831.da@gmail.com>
2024-09-14 22:06:39 -0300

    novas imagens

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,6 @@
 const gulp = require('gulp'); // aqui são as importações no caso do gulp e em baixo a do sass
 const sass = require('gulp-sass')(require('sass'));
+const imagemin = require('gulp-imagemin');
 
 function styles() {
     return gulp.src('./src/styles/*.scss') // gulp.src('./src/styles/*.scss')essa função serve para recuperar os arquivos
@@ -7,7 +8,14 @@ function styles() {
         .pipe(gulp.dest('./dist/css')); // aqui irá criar essas pastas
 }
 
-exports.default = styles;
+function images() {
+    return gulp.src('./src/images/**/*') // gulp.src('./src/styles/*.scss')essa função serve para recuperar os arquivos
+        .pipe(imagemin()) // aqui encadeia a compilação da linha 5 em cima. compressed serve para comprimir os arquivos
+        .pipe(gulp.dest('./dist/images')); // aqui irá criar essas pastas
+}
+
+exports.default = gulp.parallel(styles, images);
+
 exports.watch = function() {
     gulp.watch('./src/styles/*.scss', gulp.parallel(styles)) //aqui é para o programa rodar esses arquivos sem precisar ficar dando init o tempo todo. dentro do arrei serve para falar qual programa irá ser executado
 
