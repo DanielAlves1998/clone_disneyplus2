07ec7efa361d24bda8c5483b7b35cb1548c50d76
Daniel Alves <daniel199831.da@gmail.com>
2024-09-20 20:51:22 -0300

    ajustes no mobile

--- src/main.js
+++ src/main.js
@@ -1,7 +1,25 @@
 document.addEventListener('DOMContentLoaded', function() {
     const buttons = document.querySelectorAll('[data-tab-button]');//aqui é para pegar todos os botões para essa função
-    
+    const questions = document.querySelectorAll('[data-faq-question]');
 
+//aqui é a configuração para quando a rolagem passar de um ponto da página os botões aparecerem
+    const heroSection = document.querySelector('.hero');
+    const alturaHero = heroSection.clientHeight; //aqui é para pegar a autura do botão
+
+    //aqui vai pegar a barra de rolagem do eixo Y que é a vertical de cima para baixo
+    window.addEventListener('scroll', function() {
+        const posicaoAtual = window.scrollY;
+
+        //o IF significa QUANDO, nesse caso fica assim: quando posicaoAtual for menor que alturaHero 
+        if (posicaoAtual < alturaHero) {
+            ocultaElementosDoHeader(); // aqui é o chamado do elemento de baixo
+        } else {
+            exibeElementosDoHeader();
+        }
+
+    })
+
+    //seção de atraçôes, programação das abas
     for (let i = 0; i < buttons.length; i++) {
         buttons[i].addEventListener('click', function(botao) { //aqui  é para adicionar o evento com o click
             const abaAlvo = botao.target.dataset.tabButton;
@@ -12,8 +30,30 @@ document.addEventListener('DOMContentLoaded', function() {
             botao.target.classList.add('shows__tabs__button--is-active');
         })
     }
+    //seção FAQ, acordion
+// adicionando um elemento de clik
+    for (let i = 0; i < questions.length; i++) {
+        questions[i].addEventListener('click', abreOuFechaResposta);
+    }
 
 })
+// aqui adiciona a ação 
+function ocultaElementosDoHeader() {
+    const header = document.querySelector('header');
+    header.classList.add('header--is-hidden');
+}
+//aqui remove a ação
+function exibeElementosDoHeader() {
+    const header = document.querySelector('header');
+    header.classList.remove('header--is-hidden');
+}
+
+function abreOuFechaResposta(elemento) { //esse elemento no caso é o questions
+    const classe = 'faq__questions__item--is-open';
+    const elementoPai = elemento.target.parentNode;
+
+    elementoPai.classList.toggle(classe);
+}
 
 function removeBotaoAtivo() {
     const buttons = document.querySelectorAll('[data-tab-button]');
