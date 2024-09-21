39627631081dd9c77879e75474f78d07bb2011e8
Daniel Alves <daniel199831.da@gmail.com>
2024-09-20 21:08:32 -0300

    configurando o uglify

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,14 @@
 const gulp = require('gulp'); // aqui são as importações no caso do gulp e em baixo a do sass
 const sass = require('gulp-sass')(require('sass'));
 const imagemin = require('gulp-imagemin');
+const uglify = require('gulp-uglify');
+
+//aqui é para comprimir os arquivos logo depois de ter baixado o gulp-uglify
+function scripts() {
+    return gulp.src('./src/scripts/*.js')
+        .pipe(uglify())
+        .pipe(gulp.dest('./dis/js'))
+}
 
 function styles() {
     return gulp.src('./src/styles/*.scss') // gulp.src('./src/styles/*.scss')essa função serve para recuperar os arquivos
@@ -14,9 +22,9 @@ function images() {
         .pipe(gulp.dest('./dist/images')); // aqui irá criar essas pastas
 }
 
-exports.default = gulp.parallel(styles, images);
+exports.default = gulp.parallel(styles, images, scripts); //aqui é uma lista de execução, se não estiver aqui não irá ser executada
 
 exports.watch = function() {
     gulp.watch('./src/styles/*.scss', gulp.parallel(styles)) //aqui é para o programa rodar esses arquivos sem precisar ficar dando init o tempo todo. dentro do arrei serve para falar qual programa irá ser executado
-
+    gulp.watch('./src/scripts/*js', gulp.parallel(scripts))
 }
